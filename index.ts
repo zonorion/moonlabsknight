97709fee909d02edade3f5ca8373de8fc9f8e1e7
ZON <tan.m.hoang@vntrip.vn>
2021-07-30 16:58:39 +0700

    init

---
+++ index.ts
@@ -0,0 +1,43 @@
+const NAMES = ['alfred', 'alfie', 'alfie']
+
+function solve(names: string[]) : string[] {
+    const groupNames = []
+
+    //map prefix and count
+    const mapPrefix: Map<string, number> = new Map()
+
+    for (const [index, name] of names.entries()) {
+        let chars = name.split('')
+        // last char in loop of chars
+        let lastChar = ''
+        // check prefix available
+        let prefixAvail = ''
+        for (const c of chars) {
+            lastChar = lastChar + c
+            if (!mapPrefix.has(lastChar)) {
+                // check map prefix not exist last char
+                mapPrefix.set(lastChar, 1)
+                if (!prefixAvail) {
+                    prefixAvail = lastChar
+                }
+            } else {
+                // map prefix exist last char
+                mapPrefix.set(lastChar, mapPrefix.get(lastChar) + 1)
+                // if  all char of name existed
+                if (lastChar === name) {
+                    prefixAvail = `${name} ${mapPrefix.get(lastChar)}`
+                }
+            }
+        }
+        groupNames.push(prefixAvail)
+    }
+
+    return groupNames
+}
+
+function main() {
+    const res = solve(NAMES)
+    console.log((res || []).join('\n'))
+}
+
+main()
